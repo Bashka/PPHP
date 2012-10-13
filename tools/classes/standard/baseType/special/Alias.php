4974aa37f1cb4faa428f9ebdd3ba50c03cd86af4
Artur <Artur-Mamedbekov@yandex.ru>
2012-10-14 00:07:00 +0400

    new base type - Alias

---
+++ tools/classes/standard/baseType/special/Alias.php
@@ -0,0 +1,36 @@
+<?php
+namespace PPHP\tools\classes\standard\baseType\special;
+
+/**
+ * Класс-обертка служит для представления и верификации имен.
+ * Допустимый тип: только латинские буквы любого регистра, пробел и цифры
+ * ^[A-Za-z0-9 ]+$
+ */
+class Alias extends \PPHP\tools\classes\standard\baseType\wrapper{
+  protected static $type = 'alias';
+
+  /**
+   * Метод приводит переданные данные к типу обертки.
+   * @param mixed $val Приводимые данные.
+   * @return mixed Приведенные данные.
+   */
+  protected function transform($val){
+    return trim((string)$val);
+  }
+
+
+  /**
+   * Метод определяет, является ли указанное значение допустимым типом.
+   * @static
+   * @param mixed $val
+   * @return boolean true - если данные являются допустимым типом или могут быть приведены к нему без потери данных, иначе - false.
+   */
+  public static function is($val){
+    if(is_string($val)){
+      if(preg_match('/^[A-Za-z0-9 ]+$/', $val)){
+        return true;
+      }
+    }
+    return false;
+  }
+}
